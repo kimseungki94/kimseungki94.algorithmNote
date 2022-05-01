dd347c7e531adf3f90119c9d1e3bdffe47477ca7
kimseungki <skkim0832@naver.com>
2022-05-01 23:36:09 +0900

    update DoubleLinkedList

--- src/main/java/dataStructure/DoubleLinkedList.java
+++ src/main/java/dataStructure/DoubleLinkedList.java
@@ -3,7 +3,9 @@ package dataStructure;
 public class DoubleLinkedList<T> {
 
     public class Node<T> {
-
+        T data;
+        Node<T> next;
+        Node<T> prev;
     }
     public static void main(String[] args) {
 
