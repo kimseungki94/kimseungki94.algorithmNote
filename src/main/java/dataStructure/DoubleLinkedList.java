eefe81d2fd31778ec7772c9630c6da7a82e826c5
kimseungki <skkim0832@naver.com>
2022-05-02 19:10:07 +0900

    update DoubleLinkedList(insert method)

--- src/main/java/dataStructure/DoubleLinkedList.java
+++ src/main/java/dataStructure/DoubleLinkedList.java
@@ -1,13 +1,102 @@
 package dataStructure;
 
 public class DoubleLinkedList<T> {
+    Node<T> head = null;
+    Node<T> tail = null;
 
     public class Node<T> {
-        T data;
-        Node<T> next;
         Node<T> prev;
-    }
-    public static void main(String[] args) {
+        Node<T> next;
+        T data;
 
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    public void insertNode(T data) {
+        if (this.head == null) {
+            this.head = new Node<T>(data);
+            this.tail = this.head;
+        } else {
+            Node<T> node = this.head;
+            while (node.next != null) {
+                node = node.next;
+            }
+            node.next = new Node<T>(data);
+            node.next.prev = node;
+            this.tail = node.next;
+        }
+    }
+
+    public void printAll() {
+        if (this.tail != null) {
+            Node<T> node = this.tail;
+            System.out.println(node.data);
+            while (node.prev != null) {
+                node = node.prev;
+                System.out.println(node.data);
+            }
+        }
+    }
+
+    public Node<T> TsearchFromHead(T data) {
+
+        if (this.head == null) {
+            return null;
+        } else {
+            Node<T> node = this.head;
+            while (node != null) {
+                if (node.data == data) {
+                    return node;
+                }
+                node = node.next;
+            }
+            return null;
+        }
+    }
+
+    public Node<T> TsearchFromTail(T data) {
+        if (this.tail == null) {
+            return null;
+        } else {
+            Node<T> node = this.tail;
+            while (node != null) {
+                if (node.data == data) {
+                    return node;
+                }
+                node = node.prev;
+            }
+            return null;
+        }
+    }
+
+    public void insertFrontNode(T data, T isData) {
+        if (this.head.data == isData) {
+            Node<T> node = this.head;
+            node.prev = new Node<>(data);
+            node.prev.next = node;
+            node.next.prev = node;
+        } else if (this.tail != null) {
+            Node<T> node = TsearchFromTail(isData);
+            Node<T> tempNode = node.prev;
+            node.prev = new Node<>(data);
+            node.prev.prev = tempNode;
+            node.prev.next = node;
+            node.prev.prev.next = node.prev;
+        }
+    }
+
+    public static void main(String[] args) {
+        DoubleLinkedList<Integer> node = new DoubleLinkedList<>();
+        node.insertNode(1);
+        node.insertNode(2);
+        node.insertNode(3);
+        node.printAll();
+        node.insertFrontNode(10, 2);
+        node.insertFrontNode(0, 1);
+
+        System.out.println();
+        node.printAll();
     }
 }
