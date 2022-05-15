6fde2decd7e63bd76bd9f560a7775605a141603d
kimseungki <skkim0832@naver.com>
2022-05-15 15:40:49 +0900

    insert BinarySearchTreeAlgorithm algorithm

---
+++ src/main/java/dataStructure/BinarySearchTreeAlgorithm.java
@@ -0,0 +1,42 @@
+package dataStructure;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BinarySearchTreeAlgorithm {
+
+    public boolean search(ArrayList<Integer> dataList, Integer searchData) {
+        if (dataList.size() == 0) {
+            return false;
+        }
+        if (dataList.size() == 1 && searchData == dataList.get(0)) {
+            return true;
+        }
+        if (dataList.size() == 1 && searchData != dataList.get(0)) {
+            return false;
+        }
+
+        Integer medium = dataList.size() / 2;
+        if (searchData == dataList.get(medium)) {
+            return true;
+        } else {
+            if (searchData < dataList.get(medium)) {
+                return this.search(new ArrayList<>(dataList.subList(0, medium)), searchData);
+            } else {
+                return this.search(new ArrayList<>(dataList.subList(medium, dataList.size())), searchData);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int index = 0; index <= 100; index++) {
+            arrayList.add((int) (Math.random() * 100));
+        }
+        Collections.sort(arrayList);
+        System.out.println(arrayList);
+        BinarySearchTreeAlgorithm binarySearchTreeAlgorithm = new BinarySearchTreeAlgorithm();
+        System.out.println(binarySearchTreeAlgorithm.search(arrayList, 4));
+    }
+}
