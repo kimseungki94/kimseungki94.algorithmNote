db7b4a595e84868e99bfb0b78e0f0659f009c11e
kimseungki <skkim0832@naver.com>
2022-08-06 19:30:55 +0000

    review DFS

--- src/main/java/eCodingTest/binary/chap02.java
+++ src/main/java/eCodingTest/binary/chap02.java
@@ -10,62 +10,19 @@ public class chap02 {
     static int N,M;
     static int[] arr;
     static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         arr = new int[N];
-        int max=0;
+        int max = 0;
         st = new StringTokenizer(br.readLine());
-        for(int i=0;i<N;i++) {
-            arr[i]=Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int lt =0;
-        int rt=N-1;
-<<<<<<< HEAD
-        while (lt<=rt) {
-=======
-<<<<<<< HEAD
-        while (lt<rt) {
-=======
-        while (lt<=rt) {
->>>>>>> ec1d5845cdf660fd7e17737676432c77efedcbe7
-            System.out.println(lt+" "+rt);
-            int mid = (lt+rt)/2;
-            if(arr[mid]<M){
-                lt = mid+1;
-            } else {
-                rt=mid-1;
-            }
-        }
-        System.out.println(lt);
-        lt =0;
-        rt=N-1;
-<<<<<<< HEAD
-        while (lt<rt) {
-=======
-        while (lt<=rt) {
->>>>>>> ec1d5845cdf660fd7e17737676432c77efedcbe7
->>>>>>> bf416fcee00f87b8274018ddb63aa69fd158b61d
-            System.out.println(lt+" "+rt);
-            int mid = (lt+rt)/2;
-            if(arr[mid]<=M){
-                lt = mid+1;
-            } else {
-                rt=mid-1;
-            }
-        }
-<<<<<<< HEAD
-        System.out.println(lt+" "+rt);
-=======
-<<<<<<< HEAD
-        System.out.println(lt+" "+rt);
-=======
->>>>>>> ec1d5845cdf660fd7e17737676432c77efedcbe7
->>>>>>> bf416fcee00f87b8274018ddb63aa69fd158b61d
-        System.out.println(lt);
+        int lt = 0;
+        int rt = N - 1;
     }
-
 }
 
